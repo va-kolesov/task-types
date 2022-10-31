ad8985d5d0cbbfc582d9cc571374e5b46ed3b6cb
Vadim <60597212+va-kolesov@users.noreply.github.com>
2022-10-31 19:26:23 +0300

    va-kolesov/task-types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,19 @@
+type TPerson = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+
+type TEmployee = TPerson & { company: string };
+
 /*
 Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
 - name - строка
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+
+export function getPersons(): TPerson[];
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +27,4 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+export function personToString(person: TPerson | TEmployee): string;
